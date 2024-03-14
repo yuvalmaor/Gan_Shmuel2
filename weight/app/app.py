8655b5ee125b3d642803836709fd59f4793f8bdd
Yasmin Gudha <thegodha1@gmail.com>
2024-03-14 19:47:46 +0200

    initial connection with MYSQL 5.7 works

--- weight/app/app.py
+++ weight/app/app.py
@@ -1,14 +1,24 @@
 from flask import Flask
-import routes
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DATABASE_USER=os.environ.get('DATABASE_USER')
+DATABASE_NAME=os.environ.get('DATABASE_NAME')
+DATABASE_PASSWORD=os.environ.get('DATABASE_PASSWORD')
+HOST=os.environ.get('HOST')
+PORT=int(os.environ.get('PORT'))
+
 # Create the Flask application instance
 app = Flask(__name__)
-
+app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql://{DATABASE_USER}:{DATABASE_PASSWORD}@{HOST}:{PORT}/{DATABASE_NAME}"
+db = SQLAlchemy(app)
 # Define a route for the root URL ("/") to return "Hello, World!"
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
-
-# Check if the run.py script is executed directly (and not imported) and then run the app
 
 if __name__ == '__main__':
     app.run(debug=True)

843da419a649fed84297db45ee163158404c5473
OronArish <oarish96@gmail.com>
2024-03-14 15:27:17 +0200

    added app file

--- weight/app.py
+++ weight/app.py
@@ -1,13 +1,13 @@
 from flask import Flask
 
 # Create the Flask application instance
-backend = Flask(__name__)
+app = Flask(__name__)
 
 # Define a route for the root URL ("/") to return "Hello, World!"
-@backend.route('/')
+@app.route('/')
 def hello_world():
     return 'Hello, World!'
 
 # Check if the run.py script is executed directly (and not imported) and then run the app
 if __name__ == '__main__':
-    backend.run(debug=True)
+    app.run(debug=True)

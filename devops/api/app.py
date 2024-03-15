23dc609d4f18a2100f2bc0b8054bea06b63c062b
Gal <a@a.com>
2024-03-15 19:49:21 +0200

    logging

--- devops/api/app.py
+++ devops/api/app.py
@@ -1,34 +1,27 @@
 from flask import Flask,request,jsonify
-from flask.globals import g
 from swagger_ui import api_doc
-from multiprocessing import Pool,active_children,Process,Queue
-from concurrent.futures import ProcessPoolExecutor
-from tasks import gunicorn_logger,deploy
+from multiprocessing import Pool
+from tasks import gunicorn_logger,deploy,health_check
 
 
 app=Flask(__name__)
 
 app.logger.handlers = gunicorn_logger.handlers
 app.logger.setLevel(gunicorn_logger.level)
-pool=Pool(1)
+
 api_doc(app, config_path='./swagger/openapi.json', url_prefix='/api/doc', title='API doc')
-
-# def callback_a():
-#     pass
-
+pool=Pool(1)
 @app.get("/health")
 def health():
     gunicorn_logger.info("health")
-    pool.apply_async(deploy,())
-    # gunicorn_logger.info(len(active_children()))
-    #docker compose ps --format "{{.Service}} {{.State}}"
-    temp={"sevices":{"Weight": {"api":"running","database":"running"},
-                     "Billing":{"api":"running","database":"running"}}}
-    return jsonify(temp)
+    result=health_check()
+    return jsonify(result)
 
 @app.post("/trigger")
 def trigger():
+    # len(active_children())
     data=request.get_json()
+    results=pool.apply_async(deploy,())
     print(data)
     return "ok"
 
--- devops/api/tasks.py
+++ devops/api/tasks.py
@@ -1,11 +1,44 @@
 import logging
 import time
+from typing import Callable
+from uuid import uuid4
+from functools import wraps
 
 gunicorn_logger = logging.getLogger('gunicorn.error')
 
+def task(f):
+    @wraps(f)
+    def wrapper(*args, **kwds):
+        task_id=uuid4()
+        # print(f"[{task_id}] Task {f.__name__} started.")
+        gunicorn_logger.info(f"[{task_id}]Task {f.__name__} started.")
+        result=f(*args, **kwds)
+        # print(f"[{task_id}] Task {f.__name__} finished.")
+        gunicorn_logger.info(f"[{task_id}]Task {f.__name__} finished.")
+        return result
+    return wrapper
+
+def callback_task(a,*args, **kwargs):
+    pass
+
+@task
 def deploy():
     """To be implemented"""
     pass
 
+@task
 def health_check():
-    return "ok"
+    #docker compose ps --format "{{.Service}} {{.State}}"
+    return {"sevices":{"Weight": {"api":"running","database":"running"},
+                     "Billing":{"api":"running","database":"running"}}}
+
+# pool=TaskManager()
+
+if __name__ == '__main__':
+    print(health_check())
+    # with ProcessPoolExecutor(max_workers=1) as executor:
+    #     future = executor.submit(health_check)
+    #     future.add_done_callback(callback_task)
+        # print(future.result())
+    # future.add_done_callback(callback_task)
+

0f12f33c0b48f02e76834ea5bf58005991305928
Matan Peretz <matanperetz@outlook.com>
2024-03-16 13:42:12 +0200

    Finish health route to check db connection

--- weight/main.py
+++ weight/main.py
@@ -1,5 +1,5 @@
 from src.app import app
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
-    # app.run(debug=True)
+    # app.run(host='0.0.0.0')
+    app.run(debug=True)
--- weight/src/routes/health.py
+++ weight/src/routes/health.py
@@ -1,18 +1,19 @@
 from flask import Blueprint, Response
 from ..config import logger
+from ..database import db
+from sqlalchemy import text
 
 health_blueprint = Blueprint("health", __name__)
 
 
 @health_blueprint.route("/health", methods=["GET"])
 def check_health():
-    
     logger.info("Health check endpoint accessed")
-    is_database_ok = True
-    
-    if is_database_ok:
+    try:
+        db.session.execute(text("SELECT 1"))
         logger.info("Health check successful - server and database are running")
         return Response("OK", 200)
-    else:
+    except Exception as e:
         logger.critical("Health check failed - database is unreachable")
         return Response("Failure", 500)
+        

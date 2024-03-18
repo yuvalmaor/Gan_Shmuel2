48b539414771b6e9ecea19d3f570061a28e0eb23
yuval maor <yuvalmaor305@gmail.com>
2024-03-18 13:49:39 +0200

    mail fix

--- devops/yuval/deploy.py
+++ devops/yuval/deploy.py
@@ -7,7 +7,7 @@ def deploy_docker_compose(folders):
         try:
             # Run the subprocess and capture output and errors
             completed_process = subprocess.run(
-                ["docker-compose", "-f", f"{folder}/docker-compose.yml", "up", "-d"],
+                ["docker","compose", "-f", f"{folder}/docker-compose.yml", "up"],
                 check=True,
                 text=True,  # Ensure the output and errors are captured as strings
                 capture_output=True
@@ -17,7 +17,8 @@ def deploy_docker_compose(folders):
         except subprocess.CalledProcessError as e:
             # If an error occurs, append the error message to results
             results.append({"folder": folder, "output": e.stdout, "error": e.stderr})
+    print(results)
     return results
 if __name__ == "__main__":
-    folders = ["weight", "billing"]
+    folders = [ "billing"]
     deploy_docker_compose(folders)

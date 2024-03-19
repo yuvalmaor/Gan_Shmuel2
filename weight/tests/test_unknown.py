743957a28c39cb2341c612b24ff5fdc34a8b0b2a
Yasmin Gudha <thegodha1@gmail.com>
2024-03-19 10:45:01 +0200

    added one more test

--- weight/tests/test_unknown.py
+++ weight/tests/test_unknown.py
@@ -1,3 +1,7 @@
+from src.models import Container
+from src.database import db
+
+
 def test_get_unknown(client):
     response = client.get("/unknown")
 
@@ -5,6 +9,20 @@ def test_get_unknown(client):
         "CONT-00004",
         "CONT-00007"
     ]
+    response_data = response.json
+
+    assert response.status_code == 200
+    assert expected_response == response_data
+
+
+def test_get_unknown_empty_database(app, client):
+    with app.app_context():
+        Container.query.delete()
+        db.session.commit()
+
+    response = client.get("/unknown")
+
+    expected_response = []
     response_data = response.json
 
     assert response.status_code == 200

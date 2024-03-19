5a4e191639ef541b3e685bbeb6f676b10fbead7e
Yasmin Gudha <thegodha1@gmail.com>
2024-03-19 21:19:17 +0200

    added tests for health unknown and session

---
+++ weight/tests/test_.py
@@ -0,0 +1,21 @@
+import requests
+
+
+def test_get_health(remote_address):
+    response = requests.get(remote_address + '/health')
+    assert response.status_code == 200
+
+
+def test_get_unknown(remote_address):
+    response = requests.get(remote_address + '/unknown')
+    assert response.status_code == 200
+
+
+def test_get_session(remote_address):
+    response = requests.get(remote_address + '/session/aa')
+
+    expected_response = {"error": "Session not found"}
+    response_data = response.json
+
+    assert response.status_code == 404
+    assert expected_response == response_data
